ef2a1d41d595bcc4b3605042e6b20b21ea6ba905
Dalton Stewart <daltonstewart@wirelessprv-10-195-117-80.near.illinois.edu>
2022-03-23 10:55:22 -0500

    updates to evaluation.py

--- blocs/incentives/eval_by_IP.py
+++ blocs/incentives/eval_by_IP.py
@@ -17,123 +17,123 @@ import numpy as np
 import pandas as pd
 import os
 
-# tea = blc.create_sugarcane_tea() # TODO change the biorefinery and tea here
-# tea.fuel_tax = 0.
-# tea.sales_tax = 0.05785
-# tea.federal_income_tax = 0.35
-# tea.state_income_tax = 0.065
-# tea.property_tax = 0.0136
-# tea.F_investment = 1
-# tea.feedstock.price = 0.05 # TODO change feedstock price here, 0.0972 for cs
-# bst.PowerUtility.price = 0.0685
+tea = blc.create_sugarcane_tea() # TODO change the biorefinery and tea here
+tea.fuel_tax = 0.
+tea.sales_tax = 0.05785
+tea.federal_income_tax = 0.35
+tea.state_income_tax = 0.065
+tea.property_tax = 0.0136
+tea.F_investment = 1
+tea.feedstock.price = 0.05 # TODO change feedstock price here, 0.0972 for cs
+bst.PowerUtility.price = 0.0685
 # tea.state_tax_by_gross_receipts = True
 
-# model = bst.Model(tea.system, exception_hook='raise')
+model = bst.Model(tea.system, exception_hook='raise')
 
-# @model.metric(name='Utility cost', units='10^6 USD/yr')
-# def get_utility_cost():
-#     return tea.utility_cost / 1e6
+@model.metric(name='Utility cost', units='10^6 USD/yr')
+def get_utility_cost():
+    return tea.utility_cost / 1e6
 
-# @model.metric(name='Net electricity production', units='MWh/yr')
-# def get_electricity_production():
-#     return sum(i.power_utility.rate for i in tea.system.units) * tea.operating_hours/1000
+@model.metric(name='Net electricity production', units='MWh/yr')
+def get_electricity_production():
+    return sum(i.power_utility.rate for i in tea.system.units) * tea.operating_hours/1000
 
-# @model.metric(name='Ethanol production', units='gal/yr')
-# def get_ethanol_production():
-#     return tea.ethanol_product.F_mass / 2.98668849 * tea.operating_hours
+@model.metric(name='Ethanol production', units='gal/yr')
+def get_ethanol_production():
+    return tea.ethanol_product.F_mass / 2.98668849 * tea.operating_hours
 
-# @model.metric(name='Total capital investment', units='USD')
-# def get_TCI():
-#     return tea.TCI
+@model.metric(name='Total capital investment', units='USD')
+def get_TCI():
+    return tea.TCI
 
-# @model.metric(name='Ethanol equipment cost', units='USD')
-# def get_ETOH_eq():
-#     if tea.lang_factor:
-#         ethanol_eq = 1e6 * tea.lang_factor * tea.ethanol_group.get_purchase_cost()
-#     else:
-#         ethanol_eq = 1e6 * tea.ethanol_group.get_installed_cost()
-#     return ethanol_eq
+@model.metric(name='Ethanol equipment cost', units='USD')
+def get_ETOH_eq():
+    if tea.lang_factor:
+        ethanol_eq = 1e6 * tea.lang_factor * tea.ethanol_group.get_purchase_cost()
+    else:
+        ethanol_eq = 1e6 * tea.ethanol_group.get_installed_cost()
+    return ethanol_eq
 
-# @model.metric(name='Electricity equipment cost', units='USD')
-# def get_elec_eq():
-#     if tea.lang_factor:
-#         elec_eq = tea.lang_factor * tea.BT.purchase_cost if tea.BT else 0.
-#     else:
-#         elec_eq = tea.BT.installed_cost if tea.BT else 0.
-#     return elec_eq
+@model.metric(name='Electricity equipment cost', units='USD')
+def get_elec_eq():
+    if tea.lang_factor:
+        elec_eq = tea.lang_factor * tea.BT.purchase_cost if tea.BT else 0.
+    else:
+        elec_eq = tea.BT.installed_cost if tea.BT else 0.
+    return elec_eq
 
-# @model.metric(name='NM value', units='USD')
-# def get_NM_value():
-#     if tea.lang_factor:
-#         elec_eq = tea.lang_factor * tea.BT.purchase_cost if tea.BT else 0.
-#     else:
-#         elec_eq = tea.BT.installed_cost if tea.BT else 0.
-#     feedstock_value = feedstock.cost * tea.operating_hours * (tea._years + tea._start)
-#     return elec_eq + feedstock_value
+@model.metric(name='NM value', units='USD')
+def get_NM_value():
+    if tea.lang_factor:
+        elec_eq = tea.lang_factor * tea.BT.purchase_cost if tea.BT else 0.
+    else:
+        elec_eq = tea.BT.installed_cost if tea.BT else 0.
+    feedstock_value = feedstock.cost * tea.operating_hours * (tea._years + tea._start)
+    return elec_eq + feedstock_value
 
-# @model.metric(name='IA value', units='USD')
-# def get_IA_value():
-#     if tea.lang_factor:
-#         conveyor_costs = tea.lang_factor * sum([i.purchase_cost for i in tea.units if isinstance(i, bst.ConveyingBelt)])
-#     else:
-#         conveyor_costs = sum([i.installed_cost for i in tea.units if isinstance(i, bst.ConveyingBelt)])
-#     return conveyor_costs
+@model.metric(name='IA value', units='USD')
+def get_IA_value():
+    if tea.lang_factor:
+        conveyor_costs = tea.lang_factor * sum([i.purchase_cost for i in tea.units if isinstance(i, bst.ConveyingBelt)])
+    else:
+        conveyor_costs = sum([i.installed_cost for i in tea.units if isinstance(i, bst.ConveyingBelt)])
+    return conveyor_costs
 
-# @model.metric(name='Building materials', units='USD')
-# def get_building_mats():
-#     return tea.purchase_cost
+@model.metric(name='Building materials', units='USD')
+def get_building_mats():
+    return tea.purchase_cost
 
-# @model.metric(name='Assessed income tax', units='USD')
-# def get_inc_tax():
-#     return tea.state_income_tax * tea.sales
+@model.metric(name='Assessed income tax', units='USD')
+def get_inc_tax():
+    return tea.state_income_tax * tea.sales
 
-# @model.metric(name='Assessed fuel tax', units='USD')
-# def get_fuel_tax():
-#     return tea.fuel_tax * get_ethanol_production.get()
+@model.metric(name='Assessed fuel tax', units='USD')
+def get_fuel_tax():
+    return tea.fuel_tax * get_ethanol_production.get()
 
-# @model.metric(name="Baseline MFSP", units='USD/gal') #within this function, set whatever parameter values you want to use as the baseline
-# def MFSP_baseline():
-#     tea.incentive_numbers = ()
-#     MFSP_baseline_box[0] = MFSP = 2.98668849 * tea.solve_price(tea.ethanol_product)
-#     return MFSP
+@model.metric(name="Baseline MFSP", units='USD/gal') #within this function, set whatever parameter values you want to use as the baseline
+def MFSP_baseline():
+    tea.incentive_numbers = ()
+    MFSP_baseline_box[0] = MFSP = 2.98668849 * tea.solve_price(tea.ethanol_product)
+    return MFSP
 
-# MFSP_baseline_box = [None]
+MFSP_baseline_box = [None]
 
-# get_exemptions = lambda: tea.exemptions.sum()
-# get_deductions = lambda: tea.deductions.sum()
-# get_credits = lambda: tea.credits.sum()
-# get_refunds = lambda: tea.refunds.sum()
+get_exemptions = lambda: tea.exemptions.sum()
+get_deductions = lambda: tea.deductions.sum()
+get_credits = lambda: tea.credits.sum()
+get_refunds = lambda: tea.refunds.sum()
 
-# def MFSP_getter(incentive_number):
-#     def MFSP():
-#         tea.incentive_numbers = (incentive_number,)
-#         return 2.98668849 * tea.solve_price(tea.ethanol_product)
-#     return MFSP
+def MFSP_getter(incentive_number):
+    def MFSP():
+        tea.incentive_numbers = (incentive_number,)
+        return 2.98668849 * tea.solve_price(tea.ethanol_product)
+    return MFSP
 
-# def MFSP_reduction_getter(incentive_number):
-#     def MFSP():
-#         tea.incentive_numbers = (incentive_number,)
-#         return (2.98668849 * tea.solve_price(tea.ethanol_product) - MFSP_baseline_box[0])
-#     return MFSP
+def MFSP_reduction_getter(incentive_number):
+    def MFSP():
+        tea.incentive_numbers = (incentive_number,)
+        return (2.98668849 * tea.solve_price(tea.ethanol_product) - MFSP_baseline_box[0])
+    return MFSP
 
-# for incentive_number in range(1, 24):
-#     element = f"Incentive {incentive_number}"
-#     model.metric(MFSP_getter(incentive_number), 'MFSP', 'USD/gal', element)
-#     model.metric(MFSP_reduction_getter(incentive_number), 'MFSP Reduction', 'USD/gal', element)
-#     model.metric(get_exemptions, 'Exemptions', 'USD', element)
-#     model.metric(get_deductions, 'Deductions', 'USD', element)
-#     model.metric(get_credits, 'Credits', 'USD', element)
-#     model.metric(get_refunds, 'Refunds', 'USD', element)
+for incentive_number in range(1, 24):
+    element = f"Incentive {incentive_number}"
+    model.metric(MFSP_getter(incentive_number), 'MFSP', 'USD/gal', element)
+    model.metric(MFSP_reduction_getter(incentive_number), 'MFSP Reduction', 'USD/gal', element)
+    model.metric(get_exemptions, 'Exemptions', 'USD', element)
+    model.metric(get_deductions, 'Deductions', 'USD', element)
+    model.metric(get_credits, 'Credits', 'USD', element)
+    model.metric(get_refunds, 'Refunds', 'USD', element)
 
-# @model.metric(name="MFSP Reduction", units='USD/gal', element='Incentive 24')
-# def MFSP():
-#     tea.incentive_numbers = ()
-#     tea.depreciation_incentive_24(True)
-#     MFSP = 2.98668849 * tea.solve_price(tea.ethanol_product)
-#     tea.depreciation_incentive_24(False)
-#     MFSP_baseline = 2.98668849 * tea.solve_price(tea.ethanol_product)
-#     np.testing.assert_allclose(MFSP_baseline, MFSP_baseline_box[0], rtol=1e-3)
-#     return MFSP - MFSP_baseline_box[0]
+@model.metric(name="MFSP Reduction", units='USD/gal', element='Incentive 24')
+def MFSP():
+    tea.incentive_numbers = ()
+    tea.depreciation_incentive_24(True)
+    MFSP = 2.98668849 * tea.solve_price(tea.ethanol_product)
+    tea.depreciation_incentive_24(False)
+    MFSP_baseline = 2.98668849 * tea.solve_price(tea.ethanol_product)
+    np.testing.assert_allclose(MFSP_baseline, MFSP_baseline_box[0], rtol=1e-3)
+    return MFSP - MFSP_baseline_box[0]
 
 ### Create parameter distributions ============================================
 
@@ -191,10 +191,10 @@ EGeff_dist = shape.Triangle(0.7,0.85,0.9)
 # def set_state_income_tax(State_income_tax_rate):
 #     tea.state_income_tax = State_income_tax_rate
     
-# # State property tax
-# @model.parameter(element='TEA', kind='isolated', units='%', distribution=SPTR_dist)
-# def set_state_property_tax(State_property_tax_rate):
-#     tea.property_tax = State_property_tax_rate
+# State property tax
+@model.parameter(element='TEA', kind='isolated', units='%', distribution=SPTR_dist)
+def set_state_property_tax(State_property_tax_rate):
+    tea.property_tax = State_property_tax_rate
     
 # # State motor fuel tax
 # @model.parameter(element='TEA', kind='isolated', units='USD/gal', distribution=SMFTR_dist)
@@ -292,7 +292,7 @@ EGeff_dist = shape.Triangle(0.7,0.85,0.9)
 ### Perform Monte Carlo analysis ===============================================
 # model.load_default_parameters(tea.feedstock,operating_days=True)
 np.random.seed(1688)
-N_samples = 100
+N_samples = 10
 rule = 'L' # For Latin-Hypercube sampling
 samples = model.sample(N_samples, rule)
 model.load_samples(samples)
@@ -302,26 +302,26 @@ model.load_samples(samples)
 # table = model.table
 
 #Evaluate across coordinate
-# parameters = list(model.get_parameters())
-# parameters.remove(set_state_property_tax)
-# model_without = bst.Model(tea.system,
-#                           metrics=model.metrics,
-#                           parameters=parameters,
-#                           exception_hook='raise')
-# samples = model_without.sample(N_samples, rule)
-# model_without.load_samples(samples)
-# folder = os.path.dirname(__file__)
-# dct = model_without.evaluate_across_coordinate(
-#     'State prop tax',
-#     set_state_property_tax,
-#     np.linspace(
-#         set_state_property_tax.distribution.lower.min(),
-#         set_state_property_tax.distribution.upper.max(),
-#         8,
-#     ),
-#     xlfile=os.path.join(folder, 'uncertainty_across_st_prop_tax.xlsx'),
-#     notify=True
-# )
+parameters = list(model.get_parameters())
+parameters.remove(set_state_property_tax)
+model_without = bst.Model(tea.system,
+                          metrics=model.metrics,
+                          parameters=parameters,
+                          exception_hook='raise')
+samples = model_without.sample(N_samples, rule)
+model_without.load_samples(samples)
+folder = os.path.dirname(__file__)
+dct = model_without.evaluate_across_coordinate(
+    'State prop tax',
+    set_state_property_tax,
+    np.linspace(
+        set_state_property_tax.distribution.lower.min(),
+        set_state_property_tax.distribution.upper.max(),
+        8,
+    ),
+    xlfile=os.path.join(folder, 'uncertainty_across_st_prop_tax.xlsx'),
+    notify=True
+)
 
 ## Perform correlation analysis
 # sp_rho_table, sp_p_table = model.spearman_r()
--- blocs/incentives/evaluation.py
+++ blocs/incentives/evaluation.py
@@ -8,7 +8,7 @@ Examples
 
 
 """
-
+import flexsolve as flx
 import biosteam as bst
 from chaospy import distributions as shape
 import blocs as blc
@@ -524,7 +524,13 @@ def create_IPs_model(biorefinery):
         try:
             MFSP = tea.solve_price(tea.ethanol_product)
         except:
-            MFSP = tea.solve_price([tea.ethanol_product], [4])
+            original_price = tea.ethanol_product.price
+            def f(price):
+                tea.ethanol_product.price = price
+                return tea.NPV
+            (x0, x1, y0, y1) = flx.find_bracket(f, 0, 10)
+            MFSP = flx.IQ_interpolation(f, x0, x1, y0, y1, xtol=1e-3, ytol=1e4, maxiter=100000)
+            tea.ethanol_product.price = original_price
         return 2.98668849 * MFSP
     
     @model.metric(name='Utility cost', units='10^6 USD/yr')
@@ -542,6 +548,10 @@ def create_IPs_model(biorefinery):
     @model.metric(name='Total capital investment', units='USD')
     def get_TCI():
         return tea.TCI
+    
+    @model.metric(name='Fixed capital investment', units='USD')
+    def get_FCI():
+        return tea.FCI
     
     @model.metric(name='Ethanol equipment cost', units='USD')
     def get_ETOH_eq():
@@ -591,7 +601,7 @@ def create_IPs_model(biorefinery):
     @model.metric(name="Baseline MFSP", units='USD/gal') #within this function, set whatever parameter values you want to use as the baseline
     def MFSP_baseline():
         tea.incentive_numbers = ()
-        MFSP_baseline_box[0] = MFSP = 2.98668849 * tea.solve_price(tea.ethanol_product)
+        MFSP_baseline_box[0] = MFSP = solve_price()
         return MFSP
 
     MFSP_baseline_box = [None]
@@ -604,13 +614,13 @@ def create_IPs_model(biorefinery):
     def MFSP_getter(incentive_number):
         def MFSP():
             tea.incentive_numbers = (incentive_number,)
-            return 2.98668849 * tea.solve_price(tea.ethanol_product)
+            return solve_price()
         return MFSP
 
     def MFSP_reduction_getter(incentive_number):
         def MFSP():
             tea.incentive_numbers = (incentive_number,)
-            return (2.98668849 * tea.solve_price(tea.ethanol_product) - MFSP_baseline_box[0])
+            return solve_price() - MFSP_baseline_box[0]
         return MFSP
 
     for incentive_number in range(1, 21):
@@ -668,6 +678,12 @@ def create_IPs_model(biorefinery):
         pretreatment_conversions = tea.R201.reactions.X
         cofermentation_conversions = tea.R303.cofermentation.X
         saccharification_conversions = tea.R303.saccharification.X
+        
+        SPTR_dist = shape.Triangle(0.0, 0.0136, 0.04)
+        @model.parameter(element='TEA', kind='isolated', units='%', distribution=SPTR_dist) #TODO
+        def set_state_property_tax(State_property_tax_rate):
+            tea.property_tax = State_property_tax_rate
+        
        
         @param(name='Cornstover price', element=cornstover, kind='isolated', 
                units='USD/ton', baseline=cornstover.price * kg_per_ton)
@@ -813,7 +829,26 @@ def evaluate_IP(biorefinery, N=3000):
     model.load_samples(samples)
     model.evaluate()
     return model.table
-    
+
+#Evaluate across coordinate
+def evaluate_coord(biorefinery, N=10):
+    model = create_IPs_model(biorefinery)
+    parameters = list(model.get_parameters())
+    parameters.remove('[TEA] State property tax rate (%)')
+    np.random.seed(1688)
+    rule = 'L' # For Latin-Hypercube sampling
+    samples = model.sample(N, rule)
+    model.load_samples(samples)
+    return model.evaluate_across_coordinate(
+                                            '[TEA] State property tax rate (%)',
+                                            set_state_property_tax,
+                                            np.linspace(
+                                            set_state_property_tax.distribution.lower.min(),
+                                            set_state_property_tax.distribution.upper.max(),
+                                            8,),
+                                            notify=True
+                                            )
+
 # fig, ax = bst.plots.plot_spearman_1d(sp_rho_table['Biorefinery']['Baseline MFSP [USD/gal]'])
 # labels = [item.get_text() for item in ax.get_yticklabels()]
 # ax.set_yticklabels(labels)
