517bb36ee7744b226154b96deac81b0bd8616614
Dalton W. Stewart <57872565+daltonwstewart@users.noreply.github.com>
2022-12-09 09:25:00 -0600

    create CellulosicBLocSTEA class

--- blocs/incentives/incentives_tea.py
+++ blocs/incentives/incentives_tea.py
@@ -14,6 +14,11 @@ import biosteam as bst
 cs._include_blowdown_recycle = False
 cs.load()
 
+#Import state scenario data
+folder = os.path.dirname(__file__)
+st_data_file = os.path.join(folder, 'state_scenarios_for_import.xlsx')
+state_data = pd.read_excel(st_data_file, index_col=[0])
+
 __all__ = (
     'create_corn_tea',
     'create_sugarcane_tea',
@@ -396,3 +401,198 @@ class ConventionalIncentivesTEA(sc.ConventionalEthanolTEA):
     def _FOC(self, FCI):
         return (FCI*(self.property_insurance + self.maintenance + self.administration)
                 + self.labor_cost*(1+self.fringe_benefits+self.supplies))
+
+class CellulosicBLocSTEA(cs.CellulosicEthanolTEA):
+    def __init__(self, *args, incentive_numbers=(),
+                 property_tax=None,
+                 state_income_tax=None,
+                 federal_income_tax=None,
+                 ethanol_product=None,
+                 biodiesel_product=None,
+                 ethanol_group=None,
+                 biodiesel_group=None,
+                 sales_tax=None,
+                 fuel_tax=None,
+                 utility_tax=None,
+                 BT=None,
+                 feedstock=None,
+                 F_investment=1.,
+                 location=None,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.property_tax = property_tax
+        self._state_income_tax = state_income_tax
+        self.federal_income_tax = federal_income_tax
+        self.incentive_numbers = incentive_numbers
+        self.ethanol_product = ethanol_product
+        self.biodiesel_product = biodiesel_product
+        self.ethanol_group = ethanol_group
+        self.biodiesel_group = biodiesel_group
+        self.sales_tax = sales_tax
+        self.fuel_tax = fuel_tax
+        self.feedstock = feedstock
+        self.utility_tax = utility_tax
+        self.F_investment = F_investment
+        self.BT = BT
+        self.jobs_50 = 50
+        self.deduct_federal_income_tax_to_state_taxable_earnings = False
+        self.deduct_half_federal_income_tax_to_state_taxable_earnings = False
+        self.state_tax_by_gross_receipts = False
+        self.labor_cost *= (26.03/19.55) # BLS labor indices for years 2020/2007
+        
+# TODO
+        @property
+        def state_income_tax(self):
+            if self._state_income_tax: return self._state_income_tax
+            if self.location: return state_data.loc[self.location]['Income Tax Rate (decimal)']
+            return 0
+        @state_income_tax.setter
+        def state_income_tax(self, tax):
+            self._state_income_tax = tax
+
+    def depreciation_incentive_24(self, switch):
+        if switch:
+            self._depreciation_array = inc24 = self.depreciation_schedules[self.depreciation].copy()
+            inc24[0] += 0.5
+            inc24[1:] = inc24[1:] / (inc24[1:].sum() / (1 - inc24[0]))
+            np.testing.assert_allclose(inc24.sum(), 1.)
+        else:
+            self._depreciation_array = self.depreciation_schedules[self.depreciation]
+
+    def _FCI(self, TDC):
+        self._FCI_cached = FCI = self.F_investment * super()._FCI(TDC)
+        return FCI
+
+    def _fill_tax_and_incentives(self, incentives, taxable_cashflow, nontaxable_cashflow, tax, depreciation):
+        taxable_cashflow[taxable_cashflow < 0.] = 0.
+        lang_factor = self.lang_factor
+        if lang_factor:
+            converyor_costs = lang_factor * sum([i.purchase_cost for i in self.units if isinstance(i, bst.ConveyingBelt)])
+        else:
+            converyor_costs = sum([i.installed_cost for i in self.units if isinstance(i, bst.ConveyingBelt)])
+        operating_hours = self.operating_hours
+        ethanol_product = self.ethanol_product
+        biodiesel_product = self.biodiesel_product
+        biodiesel_group = self.biodiesel_group
+        ethanol_group = self.ethanol_group
+        BT = self.BT
+        fuel_value = 0.
+        if ethanol_product:
+            # Ethanol in gal/yr
+            ethanol = 2.98668849 * ethanol_product.F_mass * operating_hours
+            fuel_value += ethanol_product.cost * operating_hours
+            ethanol_eq = 1e6 * ethanol_group.get_installed_cost()
+        else:
+            ethanol = ethanol_eq = ethanol_sales = 0.
+        if biodiesel_product:
+            fuel_value += biodiesel_product.cost * operating_hours
+            if lang_factor:
+                biodiesel_eq = 1e6 * lang_factor * biodiesel_group.get_purchase_cost()
+            else:
+                biodiesel_eq = 1e6 * biodiesel_group.get_installed_cost()
+        else:
+            biodiesel_eq = 0.
+        feedstock = self.feedstock
+        feedstock_value = feedstock.cost * operating_hours
+        if lang_factor:
+            elec_eq = lang_factor * BT.purchase_cost if BT else 0.
+        else:
+            elec_eq = BT.installed_cost if BT else 0.
+        TCI = self.TCI
+        wages = self.labor_cost
+        FCI = self.FCI
+        startup_VOCfrac = self.startup_VOCfrac
+        startup_FOCfrac = self.startup_FOCfrac
+        construction_schedule = self._construction_schedule
+        taxable_property = FCI
+        start = self._start
+        years = self._years
+        w0 = self._startup_time
+        w1 = 1. - w0
+        plant_years = start + years
+        empty_cashflows = np.zeros(plant_years)
+
+        def yearly_flows(x, startup_frac):
+            y = empty_cashflows.copy()
+            y[start] = w0 * startup_frac * x + w1 * x
+            y[start + 1:] = x
+            return y
+
+        def construction_flow(x):
+            y = empty_cashflows.copy()
+            y[:start] = x * construction_schedule
+            return y
+
+        wages_arr = yearly_flows(wages, startup_FOCfrac)
+        fuel_value_arr = yearly_flows(fuel_value, startup_VOCfrac)
+        feedstock_value_arr = yearly_flows(feedstock_value, startup_VOCfrac)
+        ethanol_arr = yearly_flows(ethanol, startup_VOCfrac)
+        taxable_property_arr = (construction_flow(taxable_property) - depreciation).cumsum()
+        elec_eq_arr = construction_flow(elec_eq).cumsum()
+        biodiesel_eq_arr = construction_flow(biodiesel_eq).cumsum()
+        ethanol_eq_arr = construction_flow(ethanol_eq).cumsum()
+        converyor_cost_arr = construction_flow(converyor_costs).cumsum()
+        property_tax_arr = taxable_property_arr * self.property_tax
+        fuel_tax_arr = self.fuel_tax * fuel_value_arr
+        sales_tax = self.sales_tax
+        purchase_cost_arr = construction_flow(self.purchase_cost)
+        sales_arr = purchase_cost_arr + feedstock_value_arr
+        sales_tax_arr = None if sales_tax is None else sales_arr * sales_tax
+        util_cost_arr = yearly_flows(abs(self.utility_cost), startup_FOCfrac) # absolute value of utility cost bc it will likely always be negative
+        util_tax_arr = self.utility_tax * util_cost_arr
+        federal_assessed_income_tax = taxable_cashflow * self.federal_income_tax
+        if self.deduct_federal_income_tax_to_state_taxable_earnings:
+            state_assessed_income_tax = (taxable_cashflow - federal_assessed_income_tax) * self.state_income_tax
+        else:
+            state_assessed_income_tax = taxable_cashflow * self.state_income_tax
+        if self.deduct_half_federal_income_tax_to_state_taxable_earnings:
+            state_assessed_income_tax = (taxable_cashflow - (0.5*federal_assessed_income_tax)) * self.state_income_tax
+        else:
+            state_assessed_income_tax = taxable_cashflow * self.state_income_tax
+        if self.state_tax_by_gross_receipts:
+            revenue_arr = yearly_flows(self.sales, startup_VOCfrac)
+            state_assessed_income_tax = revenue_arr * self.state_income_tax
+        else:
+            state_assessed_income_tax = taxable_cashflow * self.state_income_tax
+        exemptions, deductions, credits, refunds = blc.determine_tax_incentives(
+            self.incentive_numbers,
+            start=self._start,
+            plant_years=self._years + self._start,
+            value_added=FCI,
+            property_taxable_value=taxable_property_arr,
+            property_tax_rate=self.property_tax,
+            biodiesel_eq=biodiesel_eq_arr,
+            ethanol_eq=ethanol_eq_arr,
+            fuel_taxable_value=fuel_value_arr,
+            fuel_tax_rate=self.fuel_tax,
+            sales_taxable_value=sales_arr, # Regards equipment cost with building materials (foundation, pipping, etc.), installation fees, and biomass flow rate
+            sales_tax_rate=self.sales_tax,
+            sales_tax_assessed=sales_tax_arr,
+            wages=wages_arr,
+            TCI=TCI,
+            ethanol=ethanol_arr,
+            fed_income_tax_assessed=federal_assessed_income_tax,
+            elec_eq=elec_eq_arr,
+            jobs_50=self.jobs_50, # Assumption made by the original lipid-cane biorefinery publication
+            utility_tax_assessed=util_tax_arr,
+            state_income_tax_assessed=state_assessed_income_tax,
+            property_tax_assessed=property_tax_arr,
+            IA_value=converyor_cost_arr,
+            building_mats=purchase_cost_arr,
+            NM_value=elec_eq + feedstock_value_arr,
+        )
+        self.exemptions = exemptions
+        self.deductions = deductions
+        self.credits = credits
+        self.refunds = refunds
+        index = taxable_cashflow > 0.
+        tax[:] = property_tax_arr + fuel_tax_arr + sales_tax_arr # util_tax_arr; utility tax not currently considered
+        tax[index] += federal_assessed_income_tax[index]
+        if self.state_tax_by_gross_receipts:
+            tax[:] += state_assessed_income_tax
+        else:
+            tax[index] += state_assessed_income_tax[index]
+        maximum_incentives = credits + refunds + deductions + exemptions
+        index = maximum_incentives > tax
+        maximum_incentives[index] = tax[index]
+        incentives[:] = maximum_incentives
