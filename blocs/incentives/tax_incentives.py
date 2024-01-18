a9d0ba1b89d1346dffcad1ee89a5f61ae380bb51
Dalton W. Stewart <57872565+daltonwstewart@users.noreply.github.com>
2024-01-18 12:26:06 -0700

    add note about expired incentives

--- blocs/incentives/tax_incentives.py
+++ blocs/incentives/tax_incentives.py
@@ -7,6 +7,12 @@ Tax Incentives
 Authors: Dalton Stewart (dalton.w.stewart@gmail.com)
          Yoel Cortes-Pena (yoelcortes@gmail.com)
 
+Users should note that tax incentives 1/E1, 8/C2, 9/C3, 10/C4, 14/C8, 15/C9, 18/R1
+are no longer in effect according to the Alternative Fuels Data Center (AFDC) and
+Database of State Incentives for Renewables and Efficiency (DSIRE). These incentives
+currently remain available for users to evaluate and no new incentives have
+been added for consideration as of January 18, 2024.
+
 """
 
 import numpy as np
@@ -34,13 +40,13 @@ def check_missing_parameter(p, name):
 
 def check_any_missing_parameter(dct, names):
     for i in names: check_missing_parameter(dct[i], i)
-        
+
 def assess_incentive(start, duration, plant_years, incentive, amount, assessed_tax, ub=None):
     start = max(start, assessed_tax.argmax())
     if start + duration > plant_years: start = plant_years - duration
     incentive[start: start + duration] = amount
     if ub is not None: incentive[incentive > ub] = ub
-    incentive = np.where( 
+    incentive = np.where(
         incentive > assessed_tax,
         assessed_tax,
         incentive
@@ -52,13 +58,13 @@ def assess_incentive_arr(start, duration, plant_years, incentive, amount, assess
     if start + duration > plant_years: start = plant_years - duration
     incentive[start: start + duration] = amount[start: start + duration]
     if ub is not None: incentive[incentive > ub] = ub
-    incentive = np.where( 
+    incentive = np.where(
         incentive > assessed_tax,
         assessed_tax,
         incentive
     )
     return incentive
-    
+
 def determine_exemption_amount(incentive_number,
                                plant_years,
                                property_taxable_value=None,
@@ -71,7 +77,7 @@ def determine_exemption_amount(incentive_number,
                                start=0):
     """
     Return 1d array of tax exemptions per year.
-    
+
     Parameters
     ----------
     incentive_number : int
@@ -79,8 +85,8 @@ def determine_exemption_amount(incentive_number,
     plant_years : int
         Number of years plant will operate.
     value_added : float, optional
-        Value added to property [$]. Assume similar to FCI. 
-    property_taxable_value : 1d array, optional 
+        Value added to property [$]. Assume similar to FCI.
+    property_taxable_value : 1d array, optional
         Value of property on which property tax can be assessed [$].
     property_tax_rate : float, optional
         Property tax rate [-].
@@ -94,7 +100,7 @@ def determine_exemption_amount(incentive_number,
         Fuel tax rate [-].
     start : int, optional
         Year incentive starts. Defaults to 0.
-    
+
     """
     lcs = locals()
     exemption = np.zeros((plant_years,))
@@ -110,28 +116,28 @@ def determine_exemption_amount(incentive_number,
         params = ('property_taxable_value', 'property_tax_rate')
         check_any_missing_parameter(lcs, params)
         duration = 10
-        exemption = assess_incentive_arr(start, duration, plant_years, exemption, property_taxable_value, property_taxable_value)       
+        exemption = assess_incentive_arr(start, duration, plant_years, exemption, property_taxable_value, property_taxable_value)
         exemption *= property_tax_rate # Exempt amount is the entire amount of state property tax assessed
     elif incentive_number == 3: #Referred to as Incentive E3 in manuscript
         params = ('ethanol_eq', 'property_taxable_value', 'property_tax_rate')
         check_any_missing_parameter(lcs, params)
         duration = 10
         exemption = assess_incentive_arr(start, duration, plant_years, exemption, ethanol_eq, property_taxable_value)
-        exemption *= property_tax_rate        
+        exemption *= property_tax_rate
     elif incentive_number == 4: #Referred to as Incentive E4 in manuscript
         params = ('fuel_tax_rate', 'property_taxable_value', 'property_tax_rate')
         check_any_missing_parameter(lcs, params)
         duration = plant_years
-        exemption = assess_incentive_arr(start, duration, plant_years, exemption, fuel_taxable_value, fuel_taxable_value)   
-        exemption *= fuel_tax_rate # Exempt amount is the entire amount of state fuel tax assessed       
+        exemption = assess_incentive_arr(start, duration, plant_years, exemption, fuel_taxable_value, fuel_taxable_value)
+        exemption *= fuel_tax_rate # Exempt amount is the entire amount of state fuel tax assessed
     elif incentive_number == 5: #Referred to as Incentive E5 in manuscript
         params = ('fuel_tax_rate', 'property_taxable_value', 'property_tax_rate')
         check_any_missing_parameter(lcs, params)
         duration = plant_years
-        exemption = assess_incentive_arr(start, duration, plant_years, exemption, property_taxable_value, property_taxable_value)       
-        exemption *= property_tax_rate # Exempt amount is the entire amount of state property tax assessed   
+        exemption = assess_incentive_arr(start, duration, plant_years, exemption, property_taxable_value, property_taxable_value)
+        exemption *= property_tax_rate # Exempt amount is the entire amount of state property tax assessed
     return exemption
-    
+
 def determine_deduction_amount(incentive_number,
                                plant_years,
                                NM_value=None,
@@ -140,7 +146,7 @@ def determine_deduction_amount(incentive_number,
                                start=0):
     """
     Return 1d array of tax deductions per year.
-    
+
     Parameters
     ----------
     incentive_number : int
@@ -148,9 +154,9 @@ def determine_deduction_amount(incentive_number,
     plant_years : int
         Number of years plant will operate.
     NM_value : 1d array, optional
-        Value of biomass boiler, gasifier, furnace, turbine-generator, 
-        storage facility, feedstock processing or drying equipment, feedstock 
-        trailer or interconnection transformer, and the value of biomass 
+        Value of biomass boiler, gasifier, furnace, turbine-generator,
+        storage facility, feedstock processing or drying equipment, feedstock
+        trailer or interconnection transformer, and the value of biomass
         materials [$/yr]
     sales_taxable_value : 1d array
         Value of purchases on which sales tax can be assessed [$/yr]
@@ -165,10 +171,10 @@ def determine_deduction_amount(incentive_number,
         params = ('NM_value', 'sales_taxable_value', 'sales_tax_rate')
         check_any_missing_parameter(lcs, params)
         duration = plant_years
-        deduction = assess_incentive_arr(start, duration, plant_years, deduction, NM_value, sales_taxable_value)       
+        deduction = assess_incentive_arr(start, duration, plant_years, deduction, NM_value, sales_taxable_value)
         deduction *= sales_tax_rate
     return deduction
-        
+
 def determine_credit_amount(incentive_number,
                             plant_years,
                             wages=None,
@@ -183,7 +189,7 @@ def determine_credit_amount(incentive_number,
                             start=0):
     """
     Return 1d array of tax credits as cash flows per year.
-    
+
     Parameters
     ----------
     incentive_number : int
@@ -196,11 +202,11 @@ def determine_credit_amount(incentive_number,
         Total capital investment [$].
     ethanol : 1d array
         Volume of ethanol produced [gal/yr].
-    fed_income_tax_assessed : 1d array, optional 
+    fed_income_tax_assessed : 1d array, optional
         Federal income tax per year [$/yr].
-    elec_eq : 1d array, optional 
+    elec_eq : 1d array, optional
         Value of equipment used for producing electricity [$].
-    jobs_50 : int, optional 
+    jobs_50 : int, optional
         Number of jobs paying more than 50,000 USD/yr.
     utility_tax_assessed : 1d array, optional
         Utility tax per year [$/yr]
@@ -210,7 +216,7 @@ def determine_credit_amount(incentive_number,
         Property tax per year [$/yr]
     start : int, optional
         Year incentive starts. Defaults to 0.
-        
+
     """
     lcs = locals()
     credit = np.zeros((plant_years,))
@@ -241,7 +247,7 @@ def determine_credit_amount(incentive_number,
     elif incentive_number == 11: #Referred to as Incentive C5 in manuscript
         params = ('state_income_tax_assessed',)
         check_any_missing_parameter(lcs, params)
-        duration = 15      
+        duration = 15
         credit = assess_incentive_arr(start, duration, plant_years, credit, state_income_tax_assessed, state_income_tax_assessed)# Credit amount is the entire amount of state income tax assessed
     elif incentive_number == 12: #Referred to as Incentive C6 in manuscript
         params = ('ethanol', 'state_income_tax_assessed')
@@ -287,7 +293,7 @@ def determine_credit_amount(incentive_number,
         credit_amount = 500 * jobs_50 # Number of jobs paying 50k+/year; DON'T MULTIPLY BY TAX RATE HERE
         duration = 5
         credit = assess_incentive(start, duration, plant_years, credit, credit_amount, state_income_tax_assessed, 1.75e5)
-    return credit   
+    return credit
 
 def determine_refund_amount(incentive_number,
                             plant_years,
@@ -300,7 +306,7 @@ def determine_refund_amount(incentive_number,
                             start=0):
     """
     Return 1d array of tax refunds as cash flows per year.
-    
+
     Parameters
     ----------
     incentive_number : int
@@ -321,7 +327,7 @@ def determine_refund_amount(incentive_number,
         State income tax per year [$/yr]
     start : int, optional
         Year incentive starts. Defaults to 0.
-        
+
     """
     lcs = locals()
     refund = np.zeros((plant_years,))
@@ -344,11 +350,11 @@ def determine_refund_amount(incentive_number,
         refund = assess_incentive_arr(start, duration, plant_years, refund, 0.2 * ethanol, state_income_tax_assessed,6e6)
         #DON'T MULTIPLY BY TAX RATE HERE
     return refund
-        
+
 def determine_tax_incentives(incentive_numbers,
                              **kwargs):
     """
-    Return a tuple of 1d arrays for tax exemptions, deductions, credits, and 
+    Return a tuple of 1d arrays for tax exemptions, deductions, credits, and
     refunds.
 
     Parameters
@@ -363,8 +369,8 @@ def determine_tax_incentives(incentive_numbers,
     plant_years : int
         Number of years plant will operate.
     value_added : float, optional
-        Value added to property [$]. Assume similar to FCI. 
-    property_taxable_value : 1d array, optional 
+        Value added to property [$]. Assume similar to FCI.
+    property_taxable_value : 1d array, optional
         Value of property on which property tax can be assessed [$/yr].
     property_tax_rate : float, optional
         Property tax rate [-].
@@ -377,9 +383,9 @@ def determine_tax_incentives(incentive_numbers,
     fuel_tax_rate : float, optional
         Fuel tax rate [-].
     NM_value : 1d array, optional
-        Value of biomass boiler, gasifier, furnace, turbine-generator, 
-        storage facility, feedstock processing or drying equipment, feedstock 
-        trailer or interconnection transformer, and the value of biomass 
+        Value of biomass boiler, gasifier, furnace, turbine-generator,
+        storage facility, feedstock processing or drying equipment, feedstock
+        trailer or interconnection transformer, and the value of biomass
         materials [$/yr].
     sales_taxable_value : 1d array
         Value of purchases on which sales tax can be assessed [$/yr]
@@ -393,11 +399,11 @@ def determine_tax_incentives(incentive_numbers,
         Total capital investment [$].
     ethanol : 1d array
         Volume of ethanol produced [gal/yr].
-    fed_income_tax_assessed : 1d array, optional 
+    fed_income_tax_assessed : 1d array, optional
         Federal income tax per year [$/yr].
-    elec_eq : 1d array, optional 
+    elec_eq : 1d array, optional
         Value of equipment used for producing electricity [$].
-    jobs_50 : int, optional 
+    jobs_50 : int, optional
         Number of jobs paying more than 50,000 USD/yr.
     utility_tax_assessed : 1d array, optional
         Utility tax per year [$/yr]
@@ -409,7 +415,7 @@ def determine_tax_incentives(incentive_numbers,
         Fees paid to (sub)contractors + cost of racks, shelving, conveyors [$].
     building_mats : 1d array, optional
         Cost of building and construction materials [$].
-        
+
     Raises
     ------
     ValueError
@@ -434,22 +440,22 @@ def determine_tax_incentives(incentive_numbers,
         elif i in CREDITS: credits.append(i)
         elif i in REFUNDS: refunds.append(i)
         else: raise ValueError(f"invalid incentive number '{i}'")
-    get_kwargs = lambda params: {i: kwargs[i] for i in params if i in kwargs} 
+    get_kwargs = lambda params: {i: kwargs[i] for i in params if i in kwargs}
     exemption_kwargs = get_kwargs(EXEMPTION_PARAMETERS)
     deduction_kwargs = get_kwargs(DEDUCTION_PARAMETERS)
     credit_kwargs = get_kwargs(CREDIT_PARAMETERS)
     refund_kwargs = get_kwargs(REFUND_PARAMETERS)
     get_incentives = lambda f, nums, kwargs: sum([f(i, **kwargs) for i in nums]) if nums else f(-1, **kwargs)
-    exemptions = get_incentives(determine_exemption_amount, 
-                                exemptions, 
+    exemptions = get_incentives(determine_exemption_amount,
+                                exemptions,
                                 exemption_kwargs)
-    deductions = get_incentives(determine_deduction_amount, 
-                                deductions, 
+    deductions = get_incentives(determine_deduction_amount,
+                                deductions,
                                 deduction_kwargs)
-    credits = get_incentives(determine_credit_amount, 
-                             credits, 
+    credits = get_incentives(determine_credit_amount,
+                             credits,
                              credit_kwargs)
-    refunds = get_incentives(determine_refund_amount, 
+    refunds = get_incentives(determine_refund_amount,
                              refunds, refund_kwargs)
     return exemptions, deductions, credits, refunds
 
